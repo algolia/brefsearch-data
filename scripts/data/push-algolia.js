e1682a490a00b5c9af0c74bad6ecf81697918672
Tim Carry <tim@pixelastic.com>
2025-10-22 14:27:00 +0200

    wip

---
+++ scripts/data/push-algolia.js
@@ -0,0 +1,35 @@
+import indexing from 'algolia-indexing';
+import { absolute, glob, readJson } from 'firost';
+import { _, pMap } from 'golgoth';
+import config from '../../lib/config.js';
+
+// Generate all records from generated files
+const generatedSources = await glob('./data/generated/*.json', {
+  cwd: absolute('<gitRoot>'),
+});
+const records = [];
+await pMap(generatedSources, async (filepath) => {
+  const data = await readJson(filepath);
+  const { episode, subtitles } = data;
+  const episodeRecords = _.map(subtitles, (subtitle) => {
+    return {
+      episode,
+      subtitle,
+    };
+  });
+  records.push(...episodeRecords);
+});
+
+const credentials = {
+  appId: config.appId,
+  indexName: config.indexName,
+  apiKey: config.adminApiKey,
+};
+const settings = config.settings;
+
+indexing.verbose();
+indexing.config({
+  batchMaxSize: 100,
+});
+
+await indexing.fullAtomic(credentials, records, settings);
