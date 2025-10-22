2757a8ae0517e4cce6c834ea14dffa7a3e25179f
Tim Carry <tim@pixelastic.com>
2025-10-22 14:32:08 +0200

    Save basename in episode

--- scripts/data/generate.js
+++ scripts/data/generate.js
@@ -14,6 +14,7 @@ import { setMostReplayedScore } from '../../lib/setMostReplayedScore.js';
 await forEachEpisode(async function (episode) {
   const basename = getBasename(episode);
   const outputFilepath = absolute(`<gitRoot>/data/generated/${basename}.json`);
+  episode.basename = basename;
 
   // Popularity
   const popularityPath = await getPopularityPath(basename);
