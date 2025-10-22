a3e011d6ee40664034b8ecac76824643e5c0169c
Tim Carry <tim@pixelastic.com>
2025-10-22 12:18:36 +0200

    Add viewCount to generated data

--- scripts/data/generate.js
+++ scripts/data/generate.js
@@ -1,7 +1,7 @@
 /**
  * Reads source data files, merge them, and create the generated files
  **/
-import { absolute, writeJson } from 'firost';
+import { absolute, exists, firostError, readJson, writeJson } from 'firost';
 import { forEachEpisode, getBasename } from '../../lib/helper.js';
 import { convertVtt } from '../../lib/convertVtt.js';
 
@@ -13,9 +13,26 @@ await forEachEpisode(async function (episode) {
   const subtitlePath = absolute(
     `<gitRoot>/data/source/subtitles/${basename}.vtt`,
   );
+  if (!(await exists(subtitlePath))) {
+    throw firostError(
+      'GENERATE_NO_SUBTITLES',
+      `Subtitle file not found: ${subtitlePath}`,
+    );
+  }
   const subtitles = await convertVtt(subtitlePath);
 
   // Popularity
+  const popularityPath = absolute(
+    `<gitRoot>/data/source/popularity/${basename}.json`,
+  );
+  if (!(await exists(popularityPath))) {
+    throw firostError(
+      'GENERATE_NO_POPULARITY',
+      `Popularity file not found: ${popularityPath}`,
+    );
+  }
+  const popularity = await readJson(popularityPath);
+  episode.viewCount = popularity.viewCount;
 
   const data = {
     episode,
