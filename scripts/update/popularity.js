23c9c08bc4ca5d83a54cd1579fd01ea38cb46306
Tim Carry <tim@pixelastic.com>
2025-11-13 20:56:32 +0100

    Fail better

--- scripts/update/popularity.js
+++ scripts/update/popularity.js
@@ -24,7 +24,7 @@ await forEachEpisode(async function (episode) {
       : await getPopularityFromYtDlp(episode.id);
   } catch (err) {
     progress.failure(err.message);
-    process.exit();
+    process.exit(1);
   }
 
   // Save to file
